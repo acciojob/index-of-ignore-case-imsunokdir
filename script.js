b46e4bad4eaa1578800a48cdbe03c274dd717c72
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 14:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+	if(!s1 || !s2){
+		return -1;
+	}
+	s1 = s1.toLowerCase();
+	s2 = s2.toLowerCase(); 
+	return s1.indexOf(s2);
+}  
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
